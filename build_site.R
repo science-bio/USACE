0f07b54050d5bcc870ed07ed93b1d5e23489bbfb
science-bio <36012595+science-bio@users.noreply.github.com>
2020-04-16 19:10:05 -0600

    update

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("~/Documents/EPWater/HTML/USACE test/")
+setwd("~/Documents/GitHub/USACE/")
 
 #render your sweet site. 
 rmarkdown::render_site()
